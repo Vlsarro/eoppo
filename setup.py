8649698f454bedf2864b0e0dde7bfb4fc5b10feb
Vlsarro <drekmaro@gmail.com>
2020-04-12 16:05:01 +0300

    remove invalid python version

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
